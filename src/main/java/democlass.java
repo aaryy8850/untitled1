1f01d9a3c1255da4879f1c0d7d5f4dad6b03ab7a
Divya <divyaverekar@gmail.com>
2023-08-23 15:45:11 +0530

    First Commit

---
+++ src/main/java/democlass.java
@@ -0,0 +1,6 @@
+public class democlass {
+    public static void main(String[] args)
+    {
+        System.out.println("welcome to Democlass \nHi Avishkar, Swaroop \nGood Afternoon Everyone");
+    }
+}
